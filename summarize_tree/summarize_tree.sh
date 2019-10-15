53e329f8faf56f7aacddc1fb87084c2543259d47
hill1588 <hill1588@morris.umn.edu>
2019-10-14 19:43:12 -0500

    fix issue where the file was not passing large and small structures tests
    the problem was that the echos were backwards causing the incorrect output. script was also reduced so there was not so much to code on the screen.

--- summarize_tree/summarize_tree.sh
+++ summarize_tree/summarize_tree.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-path=$1
-
-num_regular=$(find $path -type f | wc -l | xargs)
-num_dir=$(find $path -type d| wc -l| xargs)
-
-echo "There were "$num_regular" regular files."
-echo "There were "$num_dir" directories."
+echo "There were $(find $1 -type d | wc -l) directories."
+echo "There were $(find $1 -type f | wc -l) regular files."
 
 
