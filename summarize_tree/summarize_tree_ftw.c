3789cb42debfee706d8c4023da36a90ae23b8800
hill1588 <hill1588@morris.umn.edu>
2019-10-14 19:30:16 -0500

    fix issue where the summarize_tree_ftw* files did not pass the tests
    changed the callback function so it actually defines the stuff unlike the prior commit, which forgot about it. .sh file was given executable permissions and failed on the structure tests.

--- summarize_tree/summarize_tree_ftw.c
+++ summarize_tree/summarize_tree_ftw.c
@@ -1,5 +1,23 @@
+#include <stdio.h>
+#include <sys/stat.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <dirent.h>
+#include <unistd.h>
+#include <string.h>
+#include <ftw.h>
+
+static int num_dirs, num_regular;
+
 static int callback(const char *fpath, const struct stat *sb, int typeflag) {
 	// Define stuff here
+	if (typeflag == FTW_F) {
+		num_regular++;
+	}
+	if (typeflag == FTW_D) {
+		num_dirs++;
+	}
+	return 0;
 }
 
 #define MAX_FTW_DEPTH 16
@@ -12,12 +30,12 @@ int main(int argc, char** argv) {
 		return 1;
 	}
 	num_dirs = 0;
-	num_regulars = 0;
+	num_regular = 0;
 	
 	ftw(argv[1], callback, MAX_FTW_DEPTH);
 
 	// Print out the result
-	printf("There were %d directories.\n", num_dir);
+	printf("There were %d directories.\n", num_dirs);
 	printf("There were %d regular files.\n", num_regular);
 }
 
